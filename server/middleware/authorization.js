89148f47162b1489ff914246d26d589a90314324
Kumar <kumarma@hyd-kumarma-m.ds.ad.adp.com>
2018-05-17 01:54:05 +0530

    login attemps check

--- server/middleware/authorization.js
+++ server/middleware/authorization.js
@@ -4,13 +4,22 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (cache) => {
     return (req, res, next) => {
-        console.log("authenticating.....");
         let {"x-access-token": tokenId} = req.headers;
-        let isValid = tokenId && jwt.verify(tokenId, config.SECRET_KEY);
+        try{
+            let isValid = tokenId && jwt.verify(tokenId, config.SECRET_KEY);
+            if(isValid){
+                return next();
+            }else{
+                throw new Error("invalid token");
+            }
+        }catch(err){
+            console.error(err);
+            return res.status(401).send("not authorized");
+        }
         if(isValid){
             console.log("you are good");
-            return next();
+            
         }
-        return res.status(401).send("not authorized");
+        
     }
 }
--- server/router/user.js
+++ server/router/user.js
@@ -4,13 +4,13 @@ const jwt = require("jsonwebtoken");
 const validate = require("../util/hashing");
 
 module.exports = (express, cache) => {
-    
+
     let router = express.Router();
     let safe = express.Router();
 
     safe.post("/register", (req, res, next) => {
         let user = req.body;
-        let {username: id, password} = user;
+        let { username: id, password } = user;
         let hashObj = validate.getHashPassword(password, validate.getSalt());
         Object.assign(user, hashObj);
         cache.set(id, user);
@@ -18,35 +18,44 @@ module.exports = (express, cache) => {
         next();
     });
 
-    safe.post("/login", function(req, res, next){
-        let {username, password} = req.body;
+    safe.post("/login", function (req, res, next) {
+        let { username, password } = req.body;
         let user = cache.get(username);
-        let isValid  = validate.isValidPassword(password, user);
-        if(isValid){
-            let token = jwt.sign({username: username}, config.SECRET_KEY);
+        let isValid = validate.isValidPassword(password, user);
+        if (isValid) {
+            let now = new Date().getTime();
+            if (!_isValidAttempt(user, now)) {
+                return res.status(403).send({
+                    secondsLeft: Math.round((user.attempts.lock - now) / 1000)
+                });
+            }
+            let token = jwt.sign({ data: username }, config.SECRET_KEY, { "expiresIn": 5 * 60 }); //5min
             res.status(200).send({
                 tokenId: token
             });
-        }else{
+        } else {
+            _setInvalidAttempt(user);
+            cache.set(username, user);
             res.status(404).send({
-                message: "invalid username or password"
+                message: "invalid username or password",
+                left: user.attempts.left
             });
         }
         next();
     });
 
-    router.get("/:id", function(req, res, next){
-        let {id} = req.params;
+    router.get("/:id", function (req, res, next) {
+        let { id } = req.params;
         let user = cache.get(id);
         res.status(200).send(user);
         next();
     });
 
-    router.put("/:id", function(req, res, next){
-        let {id} = req.params;
+    router.put("/:id", function (req, res, next) {
+        let { id } = req.params;
         let data = req.body;
         user = cache.get(id);
-        Object.keys(data).forEach((key)=>{
+        Object.keys(data).forEach((key) => {
             user[key] = data[key];
         });
         cache.set(id, user);
@@ -58,4 +67,20 @@ module.exports = (express, cache) => {
         protected: router,
         unprotected: safe
     };
+}
+
+function _isValidAttempt(user, now) {
+    let attempts = user.attempts || {};
+    return attempts.lock ? now > attempts.lock : true;
+}
+
+function _setInvalidAttempt(user) {
+    let attempts = user.attempts || { left: 3 };
+    if (attempts.left === 1) {
+        attempts.left = --attempts.left
+        attempts.lock = new Date().getTime() + (1000 * 60);
+    } else {
+        attempts.left = --attempts.left
+    }
+    user.attempts = attempts;
 }
